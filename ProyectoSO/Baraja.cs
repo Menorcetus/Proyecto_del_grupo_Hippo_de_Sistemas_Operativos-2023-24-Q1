7ffade5bb35caf1cb5e37f7f384375c37f87eab4
Mig-Mart <miguel.martinez.m@estudiantat.upc.edu>
2023-11-12 18:11:07 +0100

    Clases carta y baraja
    La clase carta contiene las caract. de la BDD, la baraja es una clase hecha de un número de cartas (será la mano) y un jugador propietario.
    La mano se tiene que generar a partir de una selección aleatoria de la lista de cartas que se genera inicialmente.

---
+++ ProyectoSO/Baraja.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSO
+{
+    internal class Baraja : Carta //la baraja coje las cartas
+    {
+        public int numero_cartas { get; set; }
+        public int jugador { get; set; } //hay que asignar a la baraja su jugador o user
+
+        // 1) generar una lista entera con todas las cartas para cada jugador,
+        //esas son todas las posibilidades dentro de la base de datos
+        // 2) De esa lista generamos la mano del jugador aleatoriamente teniendo en cuenta que algunas se pueden repetir
+       
+        //1
+        public void inicializar_Cartas(string mensaje)
+
+            //el mensaje es id/nombre/fuerza/tipo/repetible....
+        {
+            string[] trozos = mensaje.Split('/');
+            int l = trozos.Length;
+            Carta[] Todas = new Carta[l/5]; //en la base datos hay l cartas dividido por 5, se podrá mejorar
+            for (int i = 0; i < l/5 - 1; i=i+5)
+            {
+                Todas[i] = new Carta();
+                Todas[i].id = Convert.ToInt32(trozos[i]);
+                Todas[i].nombre = trozos[i+1];
+                Todas[i].fuerza = Convert.ToInt32(trozos[i+2]);
+                Todas[i].tipo = Convert.ToInt32(trozos[i+3]);
+                Todas[i].repetible = Convert.ToInt32(trozos[i+4]);
+
+            }
+
+        }
+
+        //2
+
+        public void Generar_Baraja()
+        {
+
+
+            Random random = new Random();
+
+
+
+        }
+
+
+    }
+
+  
+}
