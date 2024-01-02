d9ee31344d65b4206417762ecc990ac66a7e9c09
Mig-Mart <miguel.martinez.m@estudiantat.upc.edu>
2024-01-02 23:09:09 +0100

    primero

--- Servidor.c
+++ Servidor.c
@@ -590,6 +590,17 @@ int AnalizarTurno(int id_partida, ListaPartidas *Partidas, char *jugador, char *
 	return i;
 }
 
+
+int jugadorPrimero()
+// funcion que da 1 o 2 para decidir que jugador inicia el primer turno
+// --> Activa como true en cliente el booleano "accion", que por defecto es false para ambos
+{
+	srand(time(NULL)); 
+	int primero = rand()%2;
+	return primero;
+}
+	
+
 void *AtenderCliente(void *socket){
 	// Iniciamos el socket dentro del thread
 	int sock_conn;
@@ -759,12 +770,29 @@ void *AtenderCliente(void *socket){
 					pthread_mutex_unlock(&mutex);
 					if (res2 == 1){
 						// Iniciar partida
-						sprintf(respuesta, "6/%i", id_partida);
+						int primero = jugadorPrimero();
+						
 						printf("Se va a iniciar partida: %s\n", respuesta);
 						for (int i=0;i<= Partidas.partidas[id_partida].mode; i++)
-						write(Partidas.partidas[id_partida].jugadores[i].Socket,
-							respuesta, strlen(respuesta));
+							{
+								if (primero == i)
+								{
+									sprintf(respuesta, "6/%i/1", id_partida); // 1 para el que inicia la primera accion
+									write(Partidas.partidas[id_partida].jugadores[i].Socket,
+									respuesta, strlen(respuesta));
 
+								}
+
+								else
+								{
+
+									sprintf(respuesta, "6/%i/0", id_partida);
+									write(Partidas.partidas[id_partida].jugadores[i].Socket,
+									respuesta, strlen(respuesta));
+								}
+
+							}
+						
 					}
 					// Si se cancela solo lo sabra el que ha creado la partida.
 				}
