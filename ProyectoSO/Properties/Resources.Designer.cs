91ad1804f56643461003c6ff61674e1cf4e22a4a
Ivi73 <ivanpp881@gmail.com>
2024-01-04 17:26:27 +0100

    Referencias de las carrtas puestas

--- ProyectoSO/Properties/Resources.Designer.cs
+++ ProyectoSO/Properties/Resources.Designer.cs
@@ -369,15 +369,5 @@ namespace ProyectoSO.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Hippo {
-            get {
-                object obj = ResourceManager.GetObject("Hippo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
